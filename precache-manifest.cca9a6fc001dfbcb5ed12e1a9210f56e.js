9d30cf42e21e9649c5c824dc748e902a346b4147
unknown <ignaciocaro6@gmail.com>
2020-06-19 21:40:31 -0300

    Updates

---
+++ precache-manifest.cca9a6fc001dfbcb5ed12e1a9210f56e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a62a82196270487038e17347e4145f",
+    "url": "/nachov3/cv/index.html"
+  },
+  {
+    "revision": "0cfdee340afbfabbfc50",
+    "url": "/nachov3/cv/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "328e5f89774bfecb37e8",
+    "url": "/nachov3/cv/static/css/main.8e9a059f.chunk.css"
+  },
+  {
+    "revision": "0cfdee340afbfabbfc50",
+    "url": "/nachov3/cv/static/js/2.8f829310.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/nachov3/cv/static/js/2.8f829310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328e5f89774bfecb37e8",
+    "url": "/nachov3/cv/static/js/main.f6db8b2e.chunk.js"
+  },
+  {
+    "revision": "e14ea81b0288330bcaa9",
+    "url": "/nachov3/cv/static/js/runtime-main.badc295f.js"
+  },
+  {
+    "revision": "c3049fe009ead44f5e1810e81b2d01ec",
+    "url": "/nachov3/cv/static/media/TEXT.c3049fe0.png"
+  },
+  {
+    "revision": "0eb390199255f1a1f94fe02fb5fcfd3e",
+    "url": "/nachov3/cv/static/media/acercaMio.0eb39019.png"
+  },
+  {
+    "revision": "6bb8105f8c32b43837c0316d6f5eac6b",
+    "url": "/nachov3/cv/static/media/binary-2.6bb8105f.jpg"
+  },
+  {
+    "revision": "84806f32c68859784e6dd62940bf0207",
+    "url": "/nachov3/cv/static/media/cypsoft.84806f32.png"
+  },
+  {
+    "revision": "628ca6bd29e4d4884b67bea7f62404b7",
+    "url": "/nachov3/cv/static/media/estudios.628ca6bd.png"
+  },
+  {
+    "revision": "2839d14a117b9e298781b094a340a8b7",
+    "url": "/nachov3/cv/static/media/experiencia.2839d14a.png"
+  },
+  {
+    "revision": "343d9bae75e489d541bb3ce7733e558f",
+    "url": "/nachov3/cv/static/media/fotoCV.343d9bae.png"
+  },
+  {
+    "revision": "898cfbea824a4c1b945bf5087332a155",
+    "url": "/nachov3/cv/static/media/header.898cfbea.png"
+  },
+  {
+    "revision": "10a816ddaf261d951f18ffc8e4177250",
+    "url": "/nachov3/cv/static/media/uspt.10a816dd.jpg"
+  }
+]);
